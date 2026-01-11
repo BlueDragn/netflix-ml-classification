44391d7e43d52e7753d90d1fff7c6324623a5c7f
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2026-01-11 12:37:53 +0530

    Add feature preparation using one-hot encoding

--- src/train_model.py
+++ src/train_model.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
 
 #Step1
 #Load cleaned data from project 1
@@ -23,3 +26,21 @@ print("\nX shape:", X.shape)
 print("y shape",y.shape)
 
 assert X.shape[0] == y.shape[0], "Number of samples in X and y do not match."
+
+#Step 2 feature preparation
+#Identify categorical and numerical columns
+numeric_features = ["release_year"]
+categorical_features = ["rating", "duration", "listed_in"]
+
+#create the preprocessor pipeline
+preprocessor = ColumnTransformer( transformers=[
+    ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_features),
+    ("num", "passthrough", numeric_features)
+]
+)
+print(preprocessor)
+#Apply encoding to features
+X_processed = preprocessor.fit_transform(X)
+print("\nEncoded feature matrix shape:", X_processed.shape)
+
+#step 3
