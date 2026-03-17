6e3fce6c757c344f71aa01b4bed67888d70f9795
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2026-03-17 16:27:43 +0530

    Add preprocessing pipeline (ColumnTransformer) with OneHotEncoding for categorical and passthrough for numerical features

--- src/train_model_v2.py
+++ src/train_model_v2.py
@@ -6,6 +6,8 @@ Focus: improving model input representation
 
 # Step 1: Import necessary libraries
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 
 # Step 2: Load the dataset
 df = pd.read_csv("data/processes/cleaned_data.csv")
@@ -56,3 +58,24 @@ print("\nTarget (y) sample:")
 print(y.head()) # Display the first few samples of the target variable to verify its correctness.
 print("\nFeatures (X) sample:")
 print(X.head()) # Display the first few samples of the feature set to verify the new features
+
+
+# ===================================
+# Preprocessing Pipeline
+# ===================================
+
+#feature preparation
+#Identify categorical and numerical columns
+numeric_features = ["release_year","genre_count","duration_number"]
+categorical_features = ["rating", "duration_type" ]
+
+#create the preprocessor pipeline
+preprocessor = ColumnTransformer( transformers=[
+    ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_features),
+    ("num", "passthrough", numeric_features)
+]
+)
+
+#Apply encoding to features
+X_processed = preprocessor.fit_transform(X)
+print("\nEncoded feature matrix shape:", X_processed.shape) # Check the shape of the encoded feature matrix to ensure that the preprocessing step has been applied correctly and to understand the dimensionality of the input for model training.
